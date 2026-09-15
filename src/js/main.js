70032424acd05a7dc867554c2e71a221deb0a858
Aaliya <aagbetoba@byupathway.edu>
2026-09-15 15:56:41 +0100

    "commit v1.0.0"

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+import { LoadPartials } from "./utils.js";
+import '../styles/base.css'
+import '../styles/large.css'
+
+const hambtn = document.querySelector("#hamBtn")
+const year = document.querySelector("#year");
+const navlist = document.querySelector("#navList")
+const overlay  = document.querySelector("#overlay")
+const closeMenu = document.querySelector("#closeMenu")
+
+async function init(){
+
+   await LoadPartials("/src/public/partials/head.html", document.querySelector("head"));
+   year.innerHTML = new Date().getFullYear();
+
+   hambtn.addEventListener("click",()=> {
+      navlist.classList.toggle("active")
+       hambtn.classList.toggle("active")
+       overlay.classList.toggle("active")
+       document.querySelector("nav").classList.toggle("active")
+   })
+   document.querySelector("#viewProject").addEventListener("click",()=> {
+      window.location.href = '#projects'
+   })
+   function closeMenuFunc(){
+      navlist.classList.remove("active")
+      hambtn.classList.remove("active")
+      overlay.classList.remove("active")
+      document.querySelector("nav").classList.remove("active")
+   }
+   closeMenu.addEventListener("click", closeMenuFunc)
+   overlay.addEventListener("click",closeMenuFunc)
+}
+init();
---
+++ src/js/utils.js
@@ -0,0 +1,5 @@
+export async function LoadPartials (path,element) {
+    let stringData = await fetch(path);
+    let data = await stringData.text();
+    element.insertAdjacentHTML('AfterBegin', data);
+}
