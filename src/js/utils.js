1c9dcd5fae9dee4b347cf359459eebf3363330bd
Aaliya <aagbetoba@byupathway.edu>
2026-09-15 16:17:49 +0100

    commit

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,5 @@
 export async function LoadPartials (path,element) {
     let stringData = await fetch(path);
     let data = await stringData.text();
-    element.insertAdjacentHTML('AfterBegin', data);
+    element.insertAdjacentHTML('afterBegin', data);
 }
